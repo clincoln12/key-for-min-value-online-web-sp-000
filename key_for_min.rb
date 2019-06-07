884e71d9f5afa98b8d72093ed1dd2b35e84741f5
Chris Lincoln <chris.lincoln12@gmail.com>
2019-06-07 14:15:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,11 @@
 # name_hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
-  lowest_value = name_hash
+  lowest_value = name_hash.first[1]
   lowest_key = nil
   
   name_hash.each do |key, value|
-    if lowest_value == 0 || value < lowest_value
+    if value < lowest_value
       lowest_value = value 
       lowest_key = key
     end
