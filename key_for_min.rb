c7f3f8e035f3c6281b03671b32094bae6d8c84f7
Chris Lincoln <chris.lincoln12@gmail.com>
2019-06-07 14:20:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -24,3 +24,36 @@ the point is you just set a FIRST value that gets cleared on the FIRST iteration
 this conditional works no matter what on the FIRST TRY ONLY
 
 because it gets REASSIGNED after the first run
+
+ok that makees sense now, i had no idea that it was only for the first try only
+
+yea you set it to something so it runs and sets the correct value, then it can keep running
+and see if any value is smaller than that value
+
+the idea is that you're looking for the lowest value right? so:
+
+- assign ANY value in the hash to lowest_value on the first try
+- keep comparing that value to other values. if anything is smaller, it will take lowest_value's place
+
+so if its 500, 252, 321, 4, and 3
+
+if you do lowest_value = 321
+  
+  its not zero anymore so this doesnt get triggered
+  
+  and 252, 4, and 3 would get triggered and get assigned again
+  
+  lets say 252 gets assigned next
+  then 3 would be iterated, and assigned as the lowest
+  then 4 would be iterated, and nothing would happen because 3 < 4
+  
+  do you understand yep i 100% get iut now why not 120% kill oyurself hup
+  
+  my dad used to tell me "why not 100" when i got 97 on exams srs wowhahaha
+  did you legit just jajaja stop. Ali rubbing off on me chrispictopher. she calls me "Chreestopher" HAHAHAHAHAHAHA srs? just to annoy me lol ok good
+  
+  they do the accent so well on south park ok  im gonna work
+  
+  anything else houndy
+   
+   thats it <3 danku let me know if anything else comes up and ill help
