cffb2f8d07d263b5f95d89f847f2424647a65e30
Chris Lincoln <chris.lincoln12@gmail.com>
2019-06-07 14:05:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -15,3 +15,17 @@ def key_for_min_value(name_hash)
   end
   lowest_key
 end
+
+#if the lowest_value is in it's intial state  
+#or if the value of the current key is less than the lowest_value
+#then: set the lowest_value to value and lowest_key to key
+
+how are u good how r u
+
+hows ur mom
+
+good
+
+how's your mom
+
+good
