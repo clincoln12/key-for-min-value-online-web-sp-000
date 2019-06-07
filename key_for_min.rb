ed530e735b6a21f60a7a71524255f3ca0327a77a
Chris Lincoln <chris.lincoln12@gmail.com>
2019-06-07 14:16:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,11 @@
 # name_hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
-  lowest_value = name_hash.first[1]
+  lowest_value = 0
   lowest_key = nil
   
   name_hash.each do |key, value|
-    if value < lowest_value
+    if lowest_value == 0 || value < lowest_value
       lowest_value = value 
       lowest_key = key
     end
@@ -19,3 +19,8 @@ end
 #if the lowest_value is in it's intial state  
 #or if the value of the current key is less than the lowest_value
 #then: set the lowest_value to value and lowest_key to key
+
+the point is you just set a FIRST value that gets cleared on the FIRST iteration because 
+this conditional works no matter what on the FIRST TRY ONLY
+
+because it gets REASSIGNED after the first run
